26745dd30c9cb1b8b0121b7376dd2064d68597ea
Choi JongGeon <zofqofhtltm2@gmail.com>
2020-09-14 23:35:03 +0900

    Update index.js

--- webpack/Module.js/index.js
+++ webpack/Module.js/index.js
@@ -1,5 +1,5 @@
 
-const { getCircleArea } = require('./mathUtil');
-
+// const { getCircleArea } = require('./mathUtil');
+import {getCircleArea} from './mathUtil';
 const result = getCircleArea(2);
 console.log(result);
