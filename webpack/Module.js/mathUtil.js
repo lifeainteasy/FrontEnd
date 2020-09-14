ea69836442cbacefdc4cfd3285940555faaab804
Choi JongGeon <zofqofhtltm2@gmail.com>
2020-09-14 23:35:05 +0900

    Update mathUtil.js

--- webpack/Module.js/mathUtil.js
+++ webpack/Module.js/mathUtil.js
@@ -2,7 +2,11 @@ const PI = 3.14;
 const getCircleArea = r => r * r * PI;
 
 
-module.exports = {
-    PI,
-    getCircleArea
-}
+ export {
+     PI,
+   getCircleArea
+ }
+
+// exports.PI = PI;
+
+// exports.getCircleArea = getCircleArea;
