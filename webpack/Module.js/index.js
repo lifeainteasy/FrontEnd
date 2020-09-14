6bb84abc5d2a3908956861fb48c9052c25c9bfea
Choi JongGeon <zofqofhtltm2@gmail.com>
2020-09-14 23:10:52 +0900

    [create] mathUtil.js
     interlock module

--- webpack/Module.js/index.js
+++ webpack/Module.js/index.js
@@ -1,5 +1,5 @@
-const PI = 3.14;
-const getCircleArea = r => r * r * PI;
+
+const { getCircleArea } = require('./mathUtil');
 
 const result = getCircleArea(2);
 console.log(result);
---
+++ webpack/Module.js/mathUtil.js
@@ -0,0 +1,8 @@
+const PI = 3.14;
+const getCircleArea = r => r * r * PI;
+
+
+module.exports = {
+    PI,
+    getCircleArea
+}
