e194208db2084c0a26acd01079269787e4c50cb8
Choi JongGeon <zofqofhtltm2@gmail.com>
2020-09-21 19:21:23 +0900

    Update get.js

--- axios/get.js
+++ axios/get.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import abd from './a.json';
 
-axios.get('https://my-json-server.typicode.com/zofqofhtltm8015/fs/user').then((Response)=>{
+axios.get('https://raw.githubusercontent.com/zofqofhtltm8015/fs/master/db.json').then((Response)=>{
     console.log(Response.data);
+    abd.abd = {a:'24'};
+    console.log(abd);
 }).catch((Error)=>{
     console.log(Error);
 })
