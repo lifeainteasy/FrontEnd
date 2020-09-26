e8b27a75e5a7ac4b436cb7a137659d75ecb3bc94
Choi JongGeon <zofqofhtltm2@gmail.com>
2020-09-26 18:13:13 +0900

    ♻[UPDATE] <get.js> about axios get methods

--- axios/get.js
+++ axios/get.js
@@ -3,8 +3,6 @@ import abd from './a.json';
 
 axios.get('https://raw.githubusercontent.com/zofqofhtltm8015/fs/master/db.json').then((Response)=>{
     console.log(Response.data);
-    abd.abd = {a:'24'};
-    console.log(abd);
 }).catch((Error)=>{
     console.log(Error);
 })
