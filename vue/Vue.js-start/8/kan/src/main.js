5fdffac6ffedc1614c4f2dbd689c9e4e4b53051f
Choi JongGeon <zofqofhtltm2@gmail.com>
2020-10-07 18:55:21 +0900

    ♻[ADD] VueX <-> Project

--- vue/Vue.js-start/8/kan/src/main.js
+++ vue/Vue.js-start/8/kan/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import store from './store/index';
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store,
 }).$mount('#app')
